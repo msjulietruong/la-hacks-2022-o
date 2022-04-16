6a89ea63b715144f4ebe9d4cd1b37609beceb51c
Chi Le <51723885+chile20@users.noreply.github.com>
2022-04-16 12:20:47 -0700

    Update dependencies
    Add Mantine UI Library and Tabler Icons Set

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { NavbarSimple } from './nav/Navbar';
 export default class AppWrapper extends React.Component {
   render() {
     return (
       <div className='app-container'>
+        <NavbarSimple></NavbarSimple>
         <Link to={'/'}>Home</Link>
         <Link to={'/about'}>About</Link>
         <Link to={'/about/subroute'}>Subcomponent</Link>
