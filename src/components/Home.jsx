2f9b1372ac4ad10c86fb695881efaa8b0e25dd5e
Chi Le <51723885+chile20@users.noreply.github.com>
2022-04-16 10:34:01 -0700

    Add Mantine (UI Library)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from '@mantine/core';
 
 const Home = (props) => (
   <div className='home'>
     Home
+    <Button>Hello world!</Button>;
   </div>
 )
 
